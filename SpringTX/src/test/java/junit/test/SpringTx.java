af39de8386c000404c5b79930cca286e53971aa5
JavaAdminTT <seyongccic@gmail.com>
2018-12-12 16:58:22 +0900

    dev_finish

--- SpringTX/src/test/java/junit/test/SpringTx.java
+++ SpringTX/src/test/java/junit/test/SpringTx.java
@@ -1,12 +1,37 @@
 package junit.test;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class SpringTx {
 
+	ApplicationContext ioc = new ClassPathXmlApplicationContext("applicationContext.xml");
+	JdbcTemplate template = ioc.getBean(JdbcTemplate.class);
+	
 	@Test
-	public void test() {
+	public void txTest() {
+		String sql = "SELECT * FROM employees";
+		List<Map<String,Object>> list = template.queryForList(sql);
+		for (Map<String, Object> map : list) {
+			System.out.println(map);
+		}
+	}
+	
+	@Test
+	public void test() throws SQLException {
 		
+		DataSource bean = ioc.getBean(DataSource.class);
+		Connection connection = bean.getConnection();
+		System.out.println(connection);
 	}
 
 }
